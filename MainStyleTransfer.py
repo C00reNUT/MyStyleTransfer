39dc40cc28c35f87228260a98a725dbd6e0ec61b
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2019-07-31 16:18:16 -0400

    added

--- MainStyleTransfer.py
+++ MainStyleTransfer.py
@@ -1,13 +1,12 @@
-#this code is my own copy
-import matplotlib.pyplot as plt
-
+#this code is my own (mostly)
 import numpy as np
 from PIL import Image
-import time
 import functools
 import tensorflow as tf
 from tensorflow.python.keras.preprocessing import image as kp_image
 from tensorflow.python.keras import models
+import pickle
+import os
 
 content_path = "./content/test.jpeg"
 style_path = "./style/test.jpg"
@@ -22,8 +21,8 @@ style_layers = ['block1_conv1',
 num_content_layers = len(content_layers)
 num_style_layers = len(style_layers)
 
-STYLE_CONTRIB = 1/num_style_layers
-CONTENT_CONTRIB = 1/num_content_layers
+STYLE_CONTRIB = 0.01/num_style_layers #feel free to change these values
+CONTENT_CONTRIB = 1000/num_content_layers
 
 def load_and_process_img(path_to_img): #loads image
     img = Image.open(path_to_img)
@@ -76,8 +75,9 @@ def load_model(): #this part is not my code
 
     return models.Model(vgg.input, model_outputs)  # creates the model using keras structure
 
-def big_loss_function(model, content, style, output):
-    style_features, image_features, output_features_style, output_features_image = get_all_features(model, content, style, output)
+def big_loss_function(model, style_features, image_features, output): #we don't make style/image features in house
+    #because they are never going to change
+    output_features_style, output_features_image = get_output_features(model, output)
     content_loss_value = 0
     style_loss_value = 0
 
@@ -92,16 +92,79 @@ def big_loss_function(model, content, style, output):
 
     big_loss = CONTENT_CONTRIB * content_loss_value + STYLE_CONTRIB * style_loss_value
 
-    return big_loss
-def get_all_features(model, image, style, output): #returns all feature extraction from the models
-    image_features = model(image)
-    style_outputs = model(style)
+    return big_loss, content_loss_value, style_loss_value
+
+def get_output_features(model, output):
     model_outputs = model(output)
 
-    output_features_style= model_outputs[:num_style_layers]
+    output_features_style = model_outputs[:num_style_layers]
     output_features_image = model_outputs[num_style_layers:]
+
+    return output_features_style, output_features_image
+
+def get_style_and_content_features(model, image, style): #returns all feature extraction from the models
+    image_features = model(image)
+    style_outputs = model(style)
 
     style_features = style_outputs[:num_style_layers]
     image_features = image_features[num_style_layers:] #allright, we have the stuff!
 
-    return style_features, image_features, output_features_style, output_features_image
+    return style_features, image_features
+
+
+def run_style_transfer(content_path, style_path, num_iterations, content_weight, style_weight):
+    model = load_model()
+    for layer in model.layers:
+        layer.trainable = False
+    image_list = list() #this is running record (for pickle1)
+
+    output_image = tf.Variable(initial_value =load_and_process_img(content_path), dtype = tf.float32)
+
+    content_image = load_and_process_img(content_path)
+    style_image = load_and_process_img(style_path)
+    style_features, content_features = get_style_and_content_features(model, content_image, style_image)
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=5, beta_1=0.99, epsilon=1e-1) #hyperparmeters are not mine
+
+    best_loss, best_img = float('inf'), None #this are initial values
+
+
+    norm_means = np.array([103.939, 116.779, 123.68]) #this section is not mine
+    min_vals = -norm_means
+    max_vals = 255 - norm_means
+
+    for i in range(num_iterations):
+        with tf.GradientTape() as tape:
+            all_loss, content_loss_value, style_loss_value = big_loss_function(model, style_features, content_features, output_image)
+
+        gradients = tape.gradient(all_loss, output_image)
+        optimizer.apply_gradients([(gradients, output_image)])
+
+        clipped = tf.clip_by_value(output_image, min_vals, max_vals) #some black magic with bounding
+
+        output_image.assign(clipped)
+
+        if all_loss < best_loss:
+            # Update best loss and best image from total loss.
+            best_loss = all_loss
+            best_img = deprocess_img(output_image.numpy())
+
+        if i % int(num_iterations/20) == 0:
+            image_list.append(deprocess_img(output_image.numpy()))
+            try:
+                os.remove("Best_Image.pkl")
+            except:
+                pass
+            dbfile = open("Best_Image.pkl", "ab")
+            pickle.dump(best_img, dbfile)
+            dbfile.close()
+
+        print('Iteration: {}'.format(i))
+        print('Total loss: {:.4e}, '
+              'style loss: {:.4e}, '
+              'content loss: {:.4e}, '
+              .format(all_loss, style_loss_value, content_loss_value))
+
+    dbfile_ = open("Image_Evolution.pkl", "ab")
+    pickle.dump(image_list, dbfile_)
+    return best_img, best_loss
